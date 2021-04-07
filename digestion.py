fd63d9210627e58c33528dab39d4b3f945a5dd8a
pd410668 <paulinaduda24@gmail.com>
2021-04-07 13:31:30 +0200

    new test files for digestion

--- digestion.py
+++ digestion.py
@@ -21,7 +21,7 @@ def digest(input_reads) -> zip:
     sub_title, sub_seq, sub_qual = [], [], []
     for title, seq, qual in input_reads:
         positions = [i for i in range(len(seq)) if seq.startswith("GATC", i)]
-        if len(positions) != 0:
+        if positions:
             if positions[0] == 0: positions.pop(0)
             for i in range(0, len(positions)+1):
                 sub_title.append(f"{i}.{title}")
