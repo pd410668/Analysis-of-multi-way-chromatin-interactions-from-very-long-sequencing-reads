aab3c25f224123a49e6abc1cfe8a95936f999c7f
pd410668 <80198325+pd410668@users.noreply.github.com>
2021-04-27 15:40:43 +0200

    Update filtering.py

--- src/py/filtering.py
+++ src/py/filtering.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import pysam
-<<<<<<< HEAD
 from math import isclose
 from prettytable import PrettyTable
 import argparse
@@ -113,30 +112,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-import itertools
-
-alignments_R1 = pysam.AlignmentFile("R1.bam", "rb")
-alignments_R2 = pysam.AlignmentFile("R2.bam", "rb")
-input_zip = itertools.zip_longest(alignments_R1, alignments_R2)
-outfile = pysam.AlignmentFile("outfile.bam", "wb", template=alignments_R1)
-
-for alignment_R1, alignment_R2 in input_zip:
-    try:
-        if alignment_R1.qname == alignment_R2.qname or alignment_R1.qname[2:] == alignment_R2.qname[2:] \
-            or alignment_R1.qname[2:] == alignment_R2.qname or alignment_R1.qname == alignment_R2.qname[2:]:
-            if alignment_R1.qname == alignment_R2.qname and alignment_R1.qname[0].isalpha():
-                outfile.write(alignment_R1)
-                outfile.write(alignment_R2)
-            # if alignment_R1.qname[0].isdigit() and alignment_R2.qname[0].isalpha():
-
-            if alignment_R2.qname[0].isdigit() and alignment_R1.qname[0].isalpha():
-                outfile.write(alignment_R1)
-
-
-    except Exception:
-        if alignment_R1 == None:
-            outfile.write(alignment_R2)
-        if alignment_R2 == None:
-            outfile.write(alignment_R1)
->>>>>>> 401be28b8b26140cbf398947e48ceb212576fa3f
