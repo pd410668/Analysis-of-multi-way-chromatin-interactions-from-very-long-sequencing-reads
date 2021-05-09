8b3da74b84963d459667f9e7884f79e7c27a8e12
pd410668 <80198325+pd410668@users.noreply.github.com>
2021-05-09 16:56:17 +0200

    final version

--- src/py/digestion.py
+++ src/py/digestion.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 """
-
 The script is taking as input fastq file. Next the digest function cuts at the point
 of occurrence under the GATC sequence and it returns the digested fastq file.
 Usage:
 ./digestion.py input.fastq output.fastq
-
 """
 
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
@@ -27,12 +25,13 @@ def digest(input_reads) -> zip:
         positions = [i for i in range(len(seq)) if seq.startswith("GATC", i)]
         if positions and positions[0] == 0:
             positions.pop(0)
-            sub_title.append(title), sub_seq.append(seq), sub_qual.append(qual)
             if positions:
                 for i in range(0, len(positions)+1):
                     sub_title.append(f"{i}.{title}")
                     sub_seq.append(division(i, seq, positions))
                     sub_qual.append(division(i, qual, positions))
+            else:
+                sub_title.append(title), sub_seq.append(seq), sub_qual.append(qual)
         else:
             sub_title.append(title), sub_seq.append(seq), sub_qual.append(qual)
     return zip(sub_title, sub_seq, sub_qual)
