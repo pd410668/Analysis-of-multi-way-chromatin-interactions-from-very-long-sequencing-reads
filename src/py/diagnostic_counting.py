f822e1b12047d2c1b8736b723b6ff15b47d39246
pd410668 <paulinaduda24@gmail.com>
2021-05-29 00:35:31 +0200

    comment extension

--- src/py/diagnostic_counting.py
+++ src/py/diagnostic_counting.py
@@ -40,6 +40,46 @@ print(df["chr_R1"].value_counts())
 """
 Output for "hs_k562_I_1"
 
+without conditions:
+
+R1 vs R2    1254288
+R1 vs R1       1144
+R2 vs R2        358
+
+R1 vs R1: max and min abs value of two positions
+243838403
+13
+
+R1 vs R2: max and min abs value of two positions
+243838440
+0
+
+R2 vs R2: max and min abs value of two positions
+219367672
+0
+
+# example atypical chromosomes found in "hs_k562_I_1"
+
+chrM                       4794
+chrUn_gl000232              495
+chrUn_gl000220              127
+chrUn_gl000234               77
+chrUn_gl000231               46
+chrUn_gl000224               37
+chrUn_gl000219               20
+chrUn_gl000240                7
+chr4_gl000194_random          3
+chr7_gl000195_random          3
+chr4_gl000193_random          2
+chr1_gl000192_random          1
+chrUn_gl000241                1
+chr19_gl000208_random         1
+chrUn_gl000238                1
+chrUn_gl000216                1
+chrUn_gl000214                1
+chr17_gl000205_random         1
+
+
 with condition: 
 # compare only between one chromosome
 # absolute value of their position does not exceed 1,000 bp
