814bef63c0ef10c0370f806baa64ea621aef652e
pd410668 <80198325+pd410668@users.noreply.github.com>
2021-05-29 19:34:26 +0200

    added a description of all the axis

--- src/py/statistics.py
+++ src/py/statistics.py
@@ -11,51 +11,53 @@ chmod 777 statistics.py
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sys
 import matplotlib.ticker as ticker
+import sys
+
 
 def main():
     def distancess_histplot(df, experiment_name):
         sns.set_style("whitegrid")
-        fig, ax = plt.subplots(2, 2, sharex=True, figsize=(16, 12))
-        plt.subplots_adjust(bottom=0.1, top=0.9, hspace=0.18, wspace=0.1, right=0.93, left=0.15)
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=False, sharey=False, figsize=(16, 12))
+        plt.subplots_adjust(bottom=0.1, top=0.9, hspace=0.25, wspace=0.3, right=0.93, left=0.15)
 
         if "R1 vs R1" in RvsR_keys:
             R1vsR1 = df["R1 vs R1"].reset_index(drop=True)
-            sns.histplot(ax=ax[0, 0], data=df, x=R1vsR1["abs_pos"], color="red", edgecolor="black")
+            sns.histplot(ax=ax1, data=df, x=R1vsR1["abs_pos"], color="red", edgecolor="black")
         else:
-            fig.delaxes(ax[0, 0])
+            fig.delaxes(ax1)
 
         if "R2 vs R2" in RvsR_keys:
             R2vsR2 = df["R2 vs R2"].reset_index(drop=True)
-            sns.histplot(ax=ax[0, 1], data=df, x=R2vsR2["abs_pos"], color="red", edgecolor="black")
+            sns.histplot(ax=ax2, data=df, x=R2vsR2["abs_pos"], color="red", edgecolor="black")
         else:
-            fig.delaxes(ax[0, 1])
+            fig.delaxes(ax2)
 
         if "R1 vs R2" in RvsR_keys:
             R1vsR2 = df["R1 vs R2"].reset_index(drop=True)
-            sns.histplot(ax=ax[1, 0], data=df, x=R1vsR2["abs_pos"], color="red", edgecolor="black")
-            ax[1, 0].yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{:,.0f}".format(x/1000) + 'K'))
+            sns.histplot(ax=ax3, data=df, x=R1vsR2["abs_pos"], color="red", edgecolor="black")
+            ax3.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{:,.0f}".format(x/1000) + 'K'))
         else:
-            fig.delaxes(ax[1, 0])
+            fig.delaxes(ax3)
 
         if "R2 vs R1" in RvsR_keys:
             R2vsR1 = df["R2 vs R1"].reset_index(drop=True)
-            sns.histplot(ax=ax[1, 1], data=df, x=R2vsR1["abs_pos"], color="red", edgecolor="black")
+            sns.histplot(ax=ax4, data=df, x=R2vsR1["abs_pos"], color="red", edgecolor="black")
         else:
-            fig.delaxes(ax[1, 1])
+            fig.delaxes(ax4)
 
-        ax[0, 0].set_title("R1 vs R1", fontsize=18)
-        ax[0, 1].set_title("R2 vs R2", fontsize=18)
-        ax[1, 0].set_title("R1 vs R2", fontsize=18)
-        ax[1, 1].set_title("R2 vs R1", fontsize=18)
+        ax1.set_title("R1 vs R1", fontsize=18)
+        ax2.set_title("R2 vs R2", fontsize=18)
+        ax3.set_title("R1 vs R2", fontsize=18)
+        ax4.set_title("R2 vs R1", fontsize=18)
 
         fig.suptitle(f"Sample from {experiment_name} human cells", fontsize=20)
-        for ax in ax.flat:
-            ax.set_xlabel("Absolute value comparing each two aligns", fontsize=18)
-            ax.set_ylabel("Number of compared aligns", fontsize=18)
-            ax.label_outer()
-        return plt.savefig(f"{sys.argv[2]}"), plt.close()
+
+        axes = [ax1, ax2, ax3]
+        for ax in axes:
+            ax.set_xlabel("Absolute value comparing each two aligns", fontsize=15)
+            ax.set_ylabel("Number of compared aligns", fontsize=15)
+        return plt.savefig(f"{sys.argv[2]}"), plt.close() 
 
     def RvsR_barplot(df, experiment_name):
         sns.set(rc={'figure.figsize': (8, 6)})
@@ -63,11 +65,11 @@ def main():
         ax = sns.barplot(x=df["RvsR"].value_counts().index, y=df["RvsR"].value_counts(), color="royalblue", edgecolor="black")
         ax.set(xlabel="Combinations", ylabel="number of matches", title=f"Sample from {experiment_name} human cells")
         ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{:,.0f}".format(x / 1000) + 'K'))
-        return plt.savefig(f"{sys.argv[3]}"), plt.close()
+        return plt.savefig(f"{sys.argv[3]}"), plt.close() 
 
 
     df = pd.read_csv(sys.argv[1], sep='\t')
-    experiment_name = sys.argv[1][26:-4]
+    experiment_name = sys.argv[1][:-4]
 
     # Initiation basic dependencies
     df_RvsR = {x: y for x, y in df.groupby("RvsR")}
