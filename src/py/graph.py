c18526884d8997ad418c8ff62ac7dfd7f39aa39b
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2021-07-16 14:35:20 +0200

    iterating aligns in selected chr
    over rest site positions around this chr

--- src/py/graph.py
+++ src/py/graph.py
@@ -2,63 +2,85 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from filtering import typical_chromosomes
 import sys
 
-def parse_positions(tsvfile) -> zip:
-    """ Returns lists of positions of aligns that are apart => 500 """
-    df = pd.read_csv(tsvfile, sep='\t')
-    clean_df = df.where(df.abs_pos >= 500).dropna().reset_index(drop=True)
-    return zip(clean_df.pos_R1.tolist(), clean_df.pos_R2.tolist())
 
-def read_headers(filename: str) -> list:
-    """ return list of headers from fasta file """
+def parse_positions(tsvfile: str, chr: str) -> zip:
+    """ Returns lists of positions of aligns of selected chromosome that are apart => 500 """
+    df = pd.read_csv(tsvfile, sep='\t')
+    clean_df = df.where(df.abs_pos >= 500).dropna().reset_index(drop=True)  # Do I need to rm aligns with abs_pos<500?
+    df_chrs = {x: y for x, y in clean_df[["chr", "pos_R1", "pos_R2"]].groupby("chr")}
+    chrs = df_chrs[chr].reset_index(drop=True)
+    return zip(chrs.pos_R1.tolist(), chrs.pos_R1.tolist())
+
+
+def read_headers(filename: str, chr: str) -> list:
+    """ return list of headers of selected chromosome from fasta file """
     from Bio import SeqIO
     from filtering import typical_chromosomes
-    return [record.id.split("-")[0] for record in SeqIO.parse(filename, "fasta") if record.id.split(":")[0] in typical_chromosomes()]
+    return [record.id.split("-")[0] for record in SeqIO.parse(filename, "fasta") if record.id.split(":")[0] == chr]
 
-def intervals(headers):
-    """ Get restriction intervals from fasta headers """
-    restriction_intervals = []
-    for i, j in zip(headers[:-1], headers[1:]):
-        res_pos_beg = int(i.split(":")[1])
-        res_pos_end = int(j.split(":")[1])
-        if res_pos_beg <= res_pos_end:
-            interval = pd.Interval(res_pos_beg, res_pos_end - 1, closed="both")
-            restriction_intervals.append(interval)
-        else:
-            interval = pd.Interval(res_pos_beg, res_pos_beg + 4, closed="both")
-            restriction_intervals.append(interval)
-        return restriction_intervals
 
-headers = read_headers(sys.argv[1])  # fasta 
-restriction_intervals = intervals(headers)
+def add_edge(u, v):
+    """ Graph construction """
+    if G.has_edge(u, v):
+        G[u][v]["weight"] += 1
+    else:
+        G.add_edge(u, v, weight=1)
 
-nodes = headers[:-1]          # list
-edges = [[i] for i in nodes]  # list of lists
 
-for pos_R1, pos_R2 in parse_positions(sys.argv[2]):  # .tsv
-    for i in range(len(restriction_intervals)):
-        if pos_R1 and pos_R2 in restriction_intervals[i]:
-            edges[i].append(nodes[i])
+def graph_construction(positions: zip):
+    right_edge = ""
+    left_edge = ""
+    for position_R1, position_R2 in positions:
+        for i, j in zip(headers[:-1], headers[1:]):  # Iterating through all intervals
+            res_pos_beg = int(i.split(":")[1])  # extracting left boundary value of interval
+            res_pos_end = int(j.split(":")[1])  # extracting right boundary value of interval
+
+            if res_pos_beg <= res_pos_end:
+                interval = pd.Interval(res_pos_beg, res_pos_end, closed="both")
             
-to_remove = []
-for edge in edges:
-    if len(edge) == 1:
-        to_remove.append(edge)
-edges = [edges.remove(i) for i in to_remove]
+                if position_R1 in interval:
+                    right_edge = f"{i}-{j}"
+                if position_R2 in interval:
+                    left_edge = f"{i}-{j}"
+                    
+            """  I can consider those intervals as irrelevant
+            else:   
+                interval = pd.Interval(res_pos_beg, res_pos_beg + 4, closed="both")
+            
+                if position_R1 in interval:
+                    right_edge = f"{i}-{j}"
+                if position_R2 in interval:
+                    left_edge = f"{i}-{j}"
+            """ 
 
-for rm in to_remove:
-    if rm[0] in nodes:
-        index = nodes.index(rm[0])
-        nodes[index] = None
+        if right_edge and left_edge:
+            add_edge(right_edge, left_edge)
 
-graph_df = pd.DataFrame({"from": nodes, "to": edges}).dropna()
 
-G = nx.from_pandas_edgelist(graph_df.explode("to"), source="from", target="to")
-nx.draw(G)
-plt.savefig("graph.png")
+G = nx.Graph()
+for chr in typical_chromosomes():
+    headers = read_headers("DpnII_hg19_seq.fa", chr)
+    positions = parse_positions("hs_k562_I_1.tsv", chr)
+    graph_construction(positions)
 
-""" Histogram of how many edges each node has (degree distribution) """
-# plt.hist([v for k, v in nx.degree(G)])
-# plt.show()
+    
+"""
+Find the node-pair for which its read coverage is maximal,
+and define this coverage as R
+"""
 
+R = (max(dict(G.edges).items(), key=lambda x: x[1]["weight"]))[1].get("weight")
+print(R)
+
+""" 
+Eliminate all other appearances of the nodes-pairs
+if their coverage is less than 0.1 × R 
+"""
+
+edge_weights = nx.get_edge_attributes(G, "weight")
+G.remove_edges_from((edge for edge, weight in edge_weights.items() if weight < 0.1 * R))
+
+print(G.edges(data=True))
