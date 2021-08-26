c0c76993e2a4e9d6f6659560bf9c14e345a25559
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2021-08-26 13:49:49 +0200

    Interval tree separate for each chrs
    output has only cwalks which is within only one chromosome

--- src/py/cwalk.py
+++ src/py/cwalk.py
@@ -9,29 +9,42 @@ import pickle
 import sys
 
 
-def parse_positions(tsvfile: str) -> zip:
+def parse_positions(tsvfile: str, abs_threshold: int) -> zip:
     """ Returns lists of positions of aligns that are apart => 500 """
     df = pd.read_csv(tsvfile, sep='\t')
-    df = df.where(df.abs_pos >= 500).dropna().reset_index(drop=True)
+    df = df.where(df.abs_pos >= abs_threshold).dropna().reset_index(drop=True)
     return zip(df.pos_R1.tolist(), df.pos_R2.tolist())
 
 
-def read_bedfile(bedfile: str) -> list:
+def read_bedfile(bedfile: str, chromosome: str) -> list:
     """ Return lists of restrictions sites positions and chromosomes where they were found """
     df = pd.read_csv(bedfile, sep="\t", header=None)
     df = df.loc[df[0].isin(typical_chromosomes())].reset_index(drop=True)
+    df = df.where(df[0] == chromosome).dropna().reset_index(drop=True)
     chr = df[0].tolist()
     pos = df[1].tolist()
     return pos, chr
 
 
 def add_edge(u, v):
-    """ Graph construction """
+    """ Creating weighted edges """
     if G.has_edge(u, v):
         G[u][v]["weight"] += 1
     else:
         G.add_edge(u, v, weight=1)
 
+        
+def matching_edges(interval_tree):
+    """ Graph construction """
+    for position_R1, position_R2 in positions:
+
+        left_edge = interval_tree[position_R1]
+        right_edge = interval_tree[position_R2]
+
+        if (left_edge and right_edge) and (left_edge != right_edge):  # prevention of empty sets and self-loops
+            add_edge(tuple(list(left_edge)[0]), tuple(list(right_edge)[0]))  # ex. (77366342.0, 77367727.0, 'chr1')
+            # print(tuple(list(left_edge)[0]), tuple(list(right_edge)[0]))
+            
 
 def cwalk(edges):
     """ Resolve the C-walk graph """
@@ -48,27 +61,19 @@ def cwalk(edges):
 
 
 if __name__ == '__main__':
-    
-    """ Initiation basic dependencies """
-    positions = parse_positions(sys.argv[1])  # .tsv file
-    restrictions, chromosomes = read_bedfile(sys.argv[2])  # .bed file
+
     G = nx.Graph()
 
-    """ Interval tree construction """
-    intervals = [(i, j, chr) for i, j, chr in zip(restrictions[:-1], restrictions[1:], chromosomes[:-1]) if i <= j]
-    tree = IntervalTree.from_tuples(intervals)
+    for chr in typical_chromosomes():
+        positions = parse_positions("hs_k562_I_1.tsv", 500)  # .tsv file
+        restrictions, chromosomes = read_bedfile("DpnII_hg19.bed", chr)  # .bed file
 
-    for position_R1, position_R2 in positions:
-
-        left_edge = tree[position_R1]
-        right_edge = tree[position_R2]
-
-        for i in list(left_edge):
-            for j in list(right_edge):
-                if i != j:  # prevention of self-loops
-                    add_edge(i, j)
+        """ Interval tree construction """  # separate for each chromosome
+        intervals = [(i, j, chr) for i, j, chr in zip(restrictions[:-1], restrictions[1:], chromosomes[:-1]) if i <= j]
+        tree = IntervalTree.from_tuples(intervals)
+        matching_edges(tree)
 
     """  C-walks construction """
     sorted_edges = sorted(G.edges(data=True), key=lambda x: x[2]["weight"], reverse=True)  # Sort edges by read-coverage
     P = cwalk(sorted_edges)
-    pickle.dump(P, open(sys.argv[3], "wb"))  # save as .txt outfile in binary mode
+    pickle.dump(P, open("cwalk.txt", "wb"))  # save as .txt outfile in binary mode
