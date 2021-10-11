71685c9141a3ddeb473153b2e7505cd75ffe1ef0
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2021-10-11 10:27:39 +0200

    Update directivity.py

--- src/py/directivity.py
+++ src/py/directivity.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
-import sys
-from tf import load_cwalk_graph
+import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+from filtering import typical_chromosomes, collect_data
+import sys
+import scipy.stats
 import statistics
+import matplotlib.ticker as ticker
+from cwalks_analysis import load_cwalk_graph, load_files
 
 
 def directionality(path: list) -> bool:
-    path = [node[0] for node in path] # the distance is between the firsts points of the restriction intervals
+    path = [node[0] for node in path]  # the distance is between the firsts points of the restriction intervals
     dist = [b - a for a, b in zip(path[:-1], path[1:])]
     same_sign_dist = [(a * b >= 0) for a, b in zip(dist[:-1], dist[1:])]
     # 0 if nodes are in the same restriction interval
@@ -18,26 +24,30 @@ def avg_directionality(signs: list) -> float:
     return round(statistics.mean(signs), 2)
 
 
-P = load_cwalk_graph("hs_k562_I_1_cwalks.txt")  # load .txt cwalk graph
+graphs, _ = load_files("./cwalks", load_cwalk_graph)  # load .txt cwalk graph
 
-signs = [directionality(list(cwalk)) for cwalk in list(nx.connected_components(P))]  # list of lists of bool values
-avg_signs = [avg_directionality(sign) for sign in signs]  # list of average value of directivity
+directions = []
+directions_avg = []
+for graph in graphs:
+    signs = [directionality(list(cwalk)) for cwalk in list(nx.connected_components(graph))]
+    avg_signs = [avg_directionality(sign) for sign in signs]
+    directions_avg.extend(avg_signs)
+    flat_signs = [sign for sublist in signs for sign in sublist]
+    directions.extend(flat_signs)
 
-plus, minus = 0, 0
-for avg_sign in avg_signs:
-    if avg_sign >= 0.5:
-        plus += 1
-    else:
-        minus += 1
 
-print(plus, minus)  # 1571 2832
+avg_sign = avg_directionality(directions)  # list of average value of directivity
+print(avg_sign)  # 0.33 - avg from one list from all cwalks graphs
+print(statistics.mean(directions_avg))  # 0.33
 
-# Student’s t-Test for independent samples
-chunks_plus = [x for x in avg_signs if x >= 0.5]
-chunks_minus = [x for x in avg_signs if x < 0.5]
 
-var_plus = round(statistics.variance(chunks_plus), 2)  # 0.05
-var_minus = round(statistics.variance(chunks_minus), 2)  # 0,02
+def hist(data):
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots(figsize=(10, 8))
+    plt.hist(data, color="cornflowerblue", edgecolor="black")
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
+    ax.set_title("Distribution of directivity of all cwalks", fontsize=18)
+    return plt.savefig("directivity.png"), plt.close()
 
-t_test = scipy.stats.ttest_ind(chunks_plus, chunks_minus, equal_var=False)
-print(t_test)  # Ttest_indResult(statistic=105.92401155048114, pvalue=0.0), the averages are significantly different
+
+hist(directions_avg)
