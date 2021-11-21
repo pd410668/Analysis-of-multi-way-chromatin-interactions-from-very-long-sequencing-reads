ef2898b4ceafdc701ca6fc5c579d6414483aa956
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2021-11-21 13:39:53 +0100

    Update boundaries.py

--- src/py/boundaries.py
+++ src/py/boundaries.py
@@ -22,9 +22,8 @@ def tad_boundaries(bedfile: str) -> dict:
 
 def main():
     graphs, _ = load_files("./cwalks", load_cwalk_graph)  # load .txt cwalk graph
-    boundaries_dict = tad_boundaries("K562_Lieberman-raw_TADs.bed")
+    boundaries_dict = tad_boundaries("K562_Lieberman-raw_TADs.bed")  # list of tuples
 
-    # prove that cwalks are in TAD boundaries
     tree_dict = dict()  # ex. tree_dict["chr1"] will be an object of type IntervalTree
     for key in boundaries_dict.keys():
         intervals = boundaries_dict[key]
@@ -32,6 +31,7 @@ def main():
         tree_dict[key] = IntervalTree.from_tuples(intervals)
 
     out_tad = 0
+    in_tad = 0
     graphs_number = 0
     cwalks_number = []
 
@@ -43,10 +43,12 @@ def main():
             if cwalk[0][2] == "chrY":
                 out_tad += 1
             else:
-                cwalk_boundaries = [[node[0], node[1]] for node in cwalk]
-                in_tad = [tree_dict[cwalk[0][2]][bound] for node in cwalk_boundaries for bound in node]
-
-                if len(in_tad) != 2 * len(cwalk_boundaries):  # if the same length, that is each node include in TAD boundaries
+                cwalk_boundaries = [((node[0] + node[1])/2) for node in cwalk]
+                itv_tad = [tree_dict[cwalk[0][2]][bound] for bound in cwalk_boundaries]
+                identical = all(element == itv_tad[0] for element in itv_tad)
+                if identical:
+                    in_tad += 1
+                else:
                     out_tad += 1
 
     print(f"In summary from each {graphs_number} graphs, we have {sum(cwalks_number)} cwalks from which {out_tad}"
@@ -55,5 +57,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # In summary from each 37 graphs, we have 319577 cwalks from which 2 are outside TAD boundaries
-    
+
+# In summary from each 37 graphs, we have 319577 cwalks from which 160346 are outside TAD boundaries
