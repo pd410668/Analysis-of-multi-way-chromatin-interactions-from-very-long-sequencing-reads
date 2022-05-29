9a5d1a3b6a26f9acad944e0ec8aa12303cd4fab9
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2022-05-29 14:27:01 +0200

    final version

--- src/py/directionality.py
+++ src/py/directionality.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
-import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import seaborn as sns
-from filtering import typical_chromosomes, collect_data
 import sys
-import scipy.stats
 import statistics
 import matplotlib.ticker as ticker
-from cwalks_analysis import load_cwalk_graph, load_files
+from analysis import load_cwalk_graph, load_files
+import random
 
 
 def directionality(path: list) -> bool:
@@ -24,51 +22,52 @@ def avg_directionality(signs: list) -> float:
     return round(statistics.mean(signs), 2)
 
 
-def hist(data, name, avg):
+def hist(data, avg, len, name):
     sns.set_style("whitegrid")
     fig, ax = plt.subplots(figsize=(10, 8))
-    plt.hist(data, color="cornflowerblue", edgecolor="black")
-    if len(data) >= 1000:
-        ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
-    ax.set_title(f"Distribution of directivity of cwalks with {name} length. \n "
+    plt.hist(data, color="royalblue", edgecolor="black")
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
+    ax.set_title(f"Distribution of directionality of cwalks of {len} length.\n "
                  f"Average value of directivity is {avg}.", fontsize=18)
-    return plt.savefig(f"directivity_{name}.png"), plt.close()
+    return plt.savefig(f"directionality_{len}_{name}.png"), plt.close()
 
 
-graphs, _ = load_files("./cwalks", load_cwalk_graph)  # load .txt cwalk graph
+def main():
+    graphs, _ = load_files(sys.argv[1], load_cwalk_graph)  # load .txt cwalk graph # "./cwalks"
 
-duplicate_cwalk_length = []
-for graph in graphs:
-    for cwalk in list(nx.connected_components(graph)):
-        duplicate_cwalk_length.append(len(cwalk))
-cwalk_length = list(set(duplicate_cwalk_length))  # list with all possible length of cwalks in each graph
-
-for each in cwalk_length:
-    print(each, duplicate_cwalk_length.count(each))
-
-for length in range(min(cwalk_length), max(cwalk_length) + 1):
-    print(length)
-
-    avg_signs_shuffle = []
-    avg_signs = []
-
+    duplicate_cwalk_length = []
     for graph in graphs:
         for cwalk in list(nx.connected_components(graph)):
-            if len(cwalk) == length:
-                cwalk = list(cwalk)
-                
-                sign = directionality(cwalk)
-                avg_sign = avg_directionality(sign)
-                
-                random.shuffle(cwalk)
-                sign_shuffle = directionality(cwalk)
-                avg_sign_shuffle = avg_directionality(sign_shuffle)
-                
-                avg_signs.append(avg_sign)
-                avg_signs_shuffle.append(avg_sign_shuffle)
+            cwalk = list(cwalk)
+            if all(cwalk[i][2] == cwalk[0][2] for i in range(0, len(cwalk))):  # intra chrs cwalks
+                duplicate_cwalk_length.append(len(cwalk))
+    cwalk_length = list(set(duplicate_cwalk_length))  # list with all possible length of cwalks in each graph
 
-    if avg_signs:
+    for length in range(min(cwalk_length), max(cwalk_length) + 1):
 
-        print(round(statistics.mean(avg_signs), 2))  # 0.33
-        print(round(statistics.mean(avg_signs_shuffle), 2))  # 0.33
-        hist(avg_signs_shuffle, length, round(statistics.mean(avg_signs_shuffle), 2))
+        avg_signs_shuffle = []
+        avg_signs = []
+
+        for graph in graphs:
+            for cwalk in list(nx.connected_components(graph)):
+                if len(cwalk) == length:
+                    cwalk = list(cwalk)
+
+                    sign = directionality(cwalk)
+                    avg_sign = avg_directionality(sign)
+
+                    random.shuffle(cwalk)
+                    sign_shuffle = directionality(cwalk)
+                    avg_sign_shuffle = avg_directionality(sign_shuffle)
+
+                    avg_signs.append(avg_sign)
+                    avg_signs_shuffle.append(avg_sign_shuffle)
+
+        if avg_signs:
+
+            hist(avg_signs_shuffle, round(statistics.mean(avg_signs_shuffle), 2), length, "shuffle")
+            hist(avg_signs, round(statistics.mean(avg_signs), 2), length, "data")
+
+
+if __name__ == '__main__':
+    main()
