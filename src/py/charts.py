647b9b2685376072b556bb4f7e4e77a595f502fa
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2022-05-29 10:25:15 +0200

    add mouse

--- src/py/charts.py
+++ src/py/charts.py
@@ -6,9 +6,11 @@ import seaborn as sns
 import matplotlib.ticker as ticker
 import sys
 
+organism = sys.argv[1]
+
 """ load .tsv file """
-df = pd.read_csv(sys.argv[1], sep='\t')
-experiment_name = sys.argv[1][21:-4]
+df = pd.read_csv(sys.argv[2], sep='\t')
+experiment_name = sys.argv[2][21:-4]
 
 """ Initiation basic dependencies """
 DIST_no_limit = df
@@ -76,7 +78,7 @@ def main():
             ax4.set_title("reverse vs forward", fontsize=18)
         else:
             fig.delaxes(ax4)
-        fig.suptitle(f"Sample from {experiment_name} human cells", fontsize=20)
+        fig.suptitle(f"Sample from {experiment_name} {organism} cells", fontsize=20)
 
         axes = [ax1, ax2, ax3, ax4]
         for ax in axes:
@@ -86,10 +88,10 @@ def main():
 
     def feature_barplot(df, experiment_name, name, save_as):
         sns.set_style("whitegrid")
-        plt.title(f"Sample from {experiment_name} human cells")
+        plt.title(f"Sample from {experiment_name} {organism} cells")
         if name == "RvsR":
             fig = plt.figure(figsize=(10, 5))
-            fig.suptitle(f"Sample from {experiment_name} human cells")
+            fig.suptitle(f"Sample from {experiment_name} {organism} cells")
             plt.subplots_adjust(wspace=0.3, top=0.85)
             fig.add_subplot(121)
             ax1 = sns.barplot(x=df[name].value_counts().index, y=df[name].value_counts(), color="royalblue", edgecolor="black")
@@ -100,7 +102,7 @@ def main():
             ax2.set(xlabel="Combinations", ylabel="Number of compared aligns")
         elif name == "strand_1vs2":
             fig = plt.figure(figsize=(8, 5))
-            fig.suptitle(f"Sample from {experiment_name} human cells")
+            fig.suptitle(f"Sample from {experiment_name} {organism} cells")
             ax = sns.barplot(x=df[name].value_counts().index, y=df[name].value_counts(), color="forestgreen", edgecolor="black")
             ax.set(xlabel="Combinations", ylabel="Number of compared aligns")
             ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
@@ -113,23 +115,23 @@ def main():
         ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
         ax.set(xlabel="Absolute value of position comparing each two aligns [log10]",
                ylabel="Number of compared aligns",
-               title=f"Sample from {experiment_name} human cells")
+               title=f"Sample from {experiment_name} {organism} cells")
         return plt.savefig(f"{name}"), plt.close()
 
     
     """ Plotting """
     # RvsR
-    feature_barplot(DIST_no_limit, experiment_name, "RvsR", sys.argv[2])  # "RvsR"
-    distances_histplot(df_RvsR_0_5000, RvsR_keys, experiment_name, sys.argv[3])  # "0_5000_R"
-    distances_histplot(df_RvsR_500_10000, RvsR_keys, experiment_name, sys.argv[4])  # "500_10000_R"
+    feature_barplot(DIST_no_limit, experiment_name, "RvsR", sys.argv[3])  # "RvsR"
+    distances_histplot(df_RvsR_0_5000, RvsR_keys, experiment_name, sys.argv[4])  # "0_5000_R"
+    distances_histplot(df_RvsR_500_10000, RvsR_keys, experiment_name, sys.argv[5])  # "500_10000_R"
 
     # strand 1vs2
-    feature_barplot(DIST_no_limit, experiment_name, "strand_1vs2", sys.argv[5])  # "strand_1vs2"
-    distances_histplot(df_strand_0_5000, strand_keys, experiment_name, sys.argv[6])  # "0_5000_S"
-    distances_histplot(df_strand_500_10000, strand_keys, experiment_name, sys.argv[7])  # "500_10000_S"
+    feature_barplot(DIST_no_limit, experiment_name, "strand_1vs2", sys.argv[6])  # "strand_1vs2"
+    distances_histplot(df_strand_0_5000, strand_keys, experiment_name, sys.argv[7])  # "0_5000_S"
+    distances_histplot(df_strand_500_10000, strand_keys, experiment_name, sys.argv[8])  # "500_10000_S"
 
     # log10-scale
-    log_scale_histplot(DIST_500_10000, experiment_name, sys.argv[8])  # log10_500_1000"
+    log_scale_histplot(DIST_500_10000, experiment_name, sys.argv[9])  # log10_500_1000"
 
 if __name__ == '__main__':
     main()
