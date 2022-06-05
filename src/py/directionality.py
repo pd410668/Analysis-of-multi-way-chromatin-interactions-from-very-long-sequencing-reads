7d4536e4798236e0c8a96e5d1fbade66237a0b34
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2022-06-05 14:23:48 +0200

    final version

--- src/py/directionality.py
+++ src/py/directionality.py
@@ -8,6 +8,8 @@ import statistics
 import matplotlib.ticker as ticker
 from analysis import load_cwalk_graph, load_files
 import random
+from filtering import typical_chromosomes 
+import pandas as pd
 
 
 def directionality(path: list) -> bool:
@@ -22,18 +24,18 @@ def avg_directionality(signs: list) -> float:
     return round(statistics.mean(signs), 2)
 
 
-def hist(data, avg, len, name):
+def hist(data, avg, name, length):
     sns.set_style("whitegrid")
     fig, ax = plt.subplots(figsize=(10, 8))
-    plt.hist(data, color="royalblue", edgecolor="black")
+    plt.hist(data, color="grey", edgecolor="black")
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
-    ax.set_title(f"Distribution of directionality of cwalks of {len} length.\n "
+    ax.set_title(f"Distribution of directionality of cwalks of {length} length from {sys.argv[1]} cells.\n "
                  f"Average value of directivity is {avg}.", fontsize=18)
-    return plt.savefig(f"directionality_{len}_{name}.png"), plt.close()
+    return plt.savefig(f"{sys.argv[1]}_directionality_{length}_{name}.png"), plt.close()
 
 
 def main():
-    graphs, _ = load_files(sys.argv[1], load_cwalk_graph)  # load .txt cwalk graph # "./cwalks"
+    graphs, _ = load_files(sys.argv[2], load_cwalk_graph)  # load .txt cwalk graph # "./cwalks"
 
     duplicate_cwalk_length = []
     for graph in graphs:
@@ -44,15 +46,17 @@ def main():
     cwalk_length = list(set(duplicate_cwalk_length))  # list with all possible length of cwalks in each graph
 
     for length in range(min(cwalk_length), max(cwalk_length) + 1):
+        print(length)
 
         avg_signs_shuffle = []
         avg_signs = []
 
         for graph in graphs:
             for cwalk in list(nx.connected_components(graph)):
+                
                 if len(cwalk) == length:
                     cwalk = list(cwalk)
-                    
+
                     if all(cwalk[i][2] == cwalk[0][2] for i in range(0, len(cwalk))):  # intra chrs cwalks
 
                         sign = directionality(cwalk)
@@ -65,10 +69,10 @@ def main():
                         avg_signs.append(avg_sign)
                         avg_signs_shuffle.append(avg_sign_shuffle)
 
-            if avg_signs:
+        if avg_signs:
 
-                hist(avg_signs_shuffle, round(statistics.mean(avg_signs_shuffle), 2), length, "shuffle")
-                hist(avg_signs, round(statistics.mean(avg_signs), 2), length, "data")
+            hist(avg_signs_shuffle, round(statistics.mean(avg_signs_shuffle), 2), "shuffle", length)
+            hist(avg_signs, round(statistics.mean(avg_signs), 2), "data", length)
 
 
 if __name__ == '__main__':
