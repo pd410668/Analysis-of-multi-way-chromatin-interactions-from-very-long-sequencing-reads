49858f44a00f54f237d26494d93ea7f1d936d104
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2022-07-24 14:01:12 +0200

    Update cwalk.py

--- src/py/cwalk.py
+++ src/py/cwalk.py
@@ -23,40 +23,38 @@ def parse_bedfile(bedfile: str, organism: str) -> dict:
     return {x: y for x, y in df.groupby(0)}
 
 
-def add_weighted_edges(u, v, node1, node2):
+def add_edge(u, v):
     """ Creating weighted edges """
     if G.has_edge(u, v):
         G[u][v]["weight"] += 1
     else:
         G.add_edge(u, v, weight=1)
-        nx.set_edge_attributes(G, {(u, v): {"node1": node1, "node2": node2}})
 
 
 def matching_edges(interval_tree_dict: dict, positions: zip):
-    """ 
+    """ Graph construction:
     interval_tree: a dictionary storing the restriction intervals (as IntervalTree) for each chromosome
     positions: list of C-walk positions
     """
     for chr1, chr2, position_R1, position_R2 in positions:
+
         left_edge = interval_tree_dict[chr1][position_R1]
         right_edge = interval_tree_dict[chr2][position_R2]
 
         if left_edge and right_edge:  # prevention of empty sets
-            left_edge = tuple(list(left_edge)[0])[0:2]
-            right_edge = tuple(list(right_edge)[0])[0:2]
-            add_weighted_edges(left_edge, right_edge, chr1, chr2)
 
+            left_edge = list(list(left_edge)[0])
+            right_edge = list(list(right_edge)[0])
+            left_edge[2], right_edge[2] = chr1, chr2
+            add_edge(tuple(left_edge), tuple(right_edge))  # ex. (77366342, 77367727, "chr1")
 
-def cwalk_construction(G):
+            
+def cwalk_construction(edges):
     """ Resolve the C-walk graph """
-    sorted_edges = sorted(G.edges(data=True), key=lambda x: x[2]["weight"], reverse=True)
     P = nx.Graph()
-    for u, v, a in sorted_edges:
-        node1 = a["node1"]
-        node2 = a["node2"]
+    for u, v, a in edges:
         if (u not in P or P.degree[u] < 2) and (v not in P or P.degree[v] < 2):
             P.add_edge(u, v, weight=a["weight"])
-            nx.set_edge_attributes(P, {(u, v): {"node1": node1, "node2": node2}})
 
     for cwalk in list(nx.connected_components(P)):
         if len(cwalk) < 3:
@@ -65,29 +63,40 @@ def cwalk_construction(G):
     return P
 
 
-def main():
-    organism = sys.argv[1]
-    restrictions_bedfile = sys.argv[2]
+def save_as_bed(graph, experiment_name):
+    numbers = list(range(1, nx.number_connected_components(graph) + 1))
+    order = [2, 0, 1]
+    for cwalk, number in zip(list(nx.connected_components(graph)), numbers):
+        cwalk_length = range(1, len(cwalk) + 1)
 
-    restrictions_dict = parse_bedfile(restrictions_bedfile, organism)  # .bed file
+        for node, length in zip(cwalk, cwalk_length):
+            node = [node[i] for i in order]
+            node.append(f"{length}_{number}")
+            collect_data(node, f"{experiment_name}", "a")
+
+
+def main():
+    restrictions_dict = parse_bedfile(sys.argv[2], sys.argv[1])  # .bed file
     tree_dict = dict()  # ex. tree_dict["chr1"] will be an object of type IntervalTree
-    for chr in typical_chromosomes(organism):
+    for chr in typical_chromosomes(sys.argv[1]):
         """ Interval tree construction, separate for each chromosome """
         restrictions = restrictions_dict[chr][1].tolist()
         intervals = [(i, j) for i, j in zip(restrictions[:-1], restrictions[1:])]
         tree_dict[chr] = IntervalTree.from_tuples(intervals)
 
+    """ Parse C-walk positions """
     positions = parse_positions(sys.argv[3], 500)  # .tsv file with selected absolute threshold
     matching_edges(tree_dict, positions)
 
-    """  cwalks construction """
-    P = cwalk_construction(G)
-    P.remove_edges_from(nx.selfloop_edges(P))  # remove self-loops
+    """  C-walks construction """
+    sorted_edges = sorted(G.edges(data=True), key=lambda x: x[2]["weight"], reverse=True)  # Sort edges by read-coverage
+    P = cwalk_construction(sorted_edges)
 
-    """ saving cwalks """
+    save_as_bed(P, sys.argv[5])  # save as .bed outfile with cwalks
     pickle.dump(P, open(sys.argv[4], "wb"))  # save cwalks as .txt outfile in binary mode
 
 
 if __name__ == '__main__':
     G = nx.Graph()
     main()
+
