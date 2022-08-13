11d1e556fd5c92b5842aa705f87e2c423f3bd8bb
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2022-08-13 13:37:10 +0200

    Update analysis.py

--- src/py/analysis.py
+++ src/py/analysis.py
@@ -1,53 +1,145 @@
 #!/usr/bin/env python
 
-from cwalks_analysis import load_cwalk_graph, load_files, histogram
-from filtering import typical_chromosomes, collect_data
 import networkx as nx
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.ticker as ticker
+from matplotlib.ticker import MaxNLocator
+import numpy as np
+import statistics
+import sys
 
-# graphs, _ = load_files("output_cadj/txt/", load_cwalk_graph)  # load cwalk graphs
-graphs, _ = load_files("output_tsv/txt/", load_cwalk_graph)
 
-i, j, k = 0, 0, 0
-inter_cwalks_len, intra_cwalks_len = [], []
-for graph in graphs:
-    print(f"self_loops: {nx.number_of_selfloops(graph)}")
-    for cwalk in list(nx.connected_components(graph)):
-        cwalk = list(cwalk)
-        # if len(cwalk) > 4: # 3 len cwalks can be removed
-        k += 1
-        if all(cwalk[i][2] == cwalk[0][2] for i in range(0, len(cwalk))):
-            inter_cwalks_len.append(len(cwalk))
-            i += 1
-        else:
-            intra_cwalks_len.append(len(cwalk))
-            j += 1
+def load_cwalk_graph(cwalk_graph):
+    import pickle
+    return pickle.load(open(cwalk_graph, "rb"))
 
-print(f"Number of intra-chromosomal cwalks: {i}")
-print(f"Number of inter-chromosomal cwalks: {j}")
-print(f"Number of all cwalks: {k}")
-print(f"Percentage f intra-chromosomal cwalks is {round((i/k)*100, 3)}%")
-print(i + j == k)
 
-"""
-k562.cadj:
-self_loops: 0
-Number of intra-chromosomal cwalks: 284686
-Number of inter-chromosomal cwalks: 73162
-Number of all cwalks: 357848
-Percentage f intra-chromosomal cwalks is 79.555%
-True
-"""
+def load_files(dictionary, parser):
+    """
+    load all available files from a folder
+    and return lists of objects
+    """
+    import os
+    files, labels = [], []
+    for file in os.listdir(dictionary):
+        labels.append(file)
+        files.append(parser(f"{dictionary}/{file}"))
+    return files, labels
 
-"""
-5 cwalk graphs: 
-self_loops: 0
-Number of intra-chromosomal cwalks: 38784
-Number of inter-chromosomal cwalks: 27159
-Number of all cwalks: 65943
-Percentage f intra-chromosomal cwalks is 58.814%
-True
-"""
 
-# Distribution of cwalks length
-histogram(inter_cwalks_len, "inter-chromosomal")
-histogram(intra_cwalks_len, "intra-chromosomal")
+def fractions(one_chr, two_chr, many_chr, name):
+    sns.set_style("whitegrid")
+    plt.figure(figsize=(12, 10))
+    bar1 = plt.bar([i for i in range(3, 16)], one_chr, width=1, edgecolor="black", color="tab:blue")
+    bar2 = plt.bar([i for i in range(3, 16)], two_chr, bottom=one_chr, width=1, edgecolor="black", color="tab:cyan")
+    bar3 = plt.bar([i for i in range(3, 16)], many_chr,
+                   bottom=np.add(one_chr, two_chr), width=1, edgecolor="black", color="tab:gray")
+    plt.xlabel("Number of hops", fontsize=16)
+    plt.ylabel("Fraction", fontsize=16)
+    plt.title(f"Fraction of c-walks in each class in {sys.argv[1]} cells", fontsize=18)
+    plt.legend([bar1, bar2, bar3], ["one chromosome (class I)",
+                                    "two chromosomes (class II)",
+                                    "many chromosomes (class III)"])
+    return plt.savefig(f"{name}"), plt.close() 
+
+
+def barh(graphs, labels, name):
+    data = []
+    for graph in graphs:
+        data.append(nx.number_connected_components(graph))
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots(figsize=(12, 16))
+    ax.barh([label[:-11] for label in labels], data, color="tab:blue", edgecolor="black", height=1)
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
+    ax.set_title(f"Number of c-walks in each graph from {sys.argv[1]} cells", fontsize=18)
+    return plt.savefig(f"{name}"), plt.close()
+
+
+def stats(graphs, name):
+    i, j, k = 0, 0, 0
+    lengths = []
+    for graph in graphs:
+        for cwalk in list(nx.connected_components(graph)):
+            cwalk = list(cwalk)
+            k += 1
+            if all(cwalk[i][2] == cwalk[0][2] for i in range(0, len(cwalk))):
+                lengths.append(len(cwalk))
+                i += 1
+            else:
+                j += 1
+                
+
+    print(f"Number of intra-chromosomal cwalks: {i}")
+    print(f"Number of inter-chromosomal cwalks: {j}")
+    print(f"Number of all cwalks: {k}")
+    print(f"Percentage of intra-chromosomal cwalks is {round((i / k) * 100, 3)}%")
+    print(f"Average: {round(statistics.mean(lengths), 2)}")
+    print(f"Median: {round(statistics.median(lengths), 2)}")
+    print(f"Mode: {statistics.mode(lengths)}")
+    print(f"Standard deviation: {round(statistics.stdev(lengths), 2)}")
+    sns.set_style("whitegrid")
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 8))
+    plt.suptitle(f"Distribution of c-walks length from {sys.argv[1]} cells", fontsize=20)
+    ax1.hist(lengths, color="tab:blue", edgecolor="black")
+    ax2.hist([x for x in lengths if x <= 6], color="tab:blue", edgecolor="black")
+    ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
+    ax2.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: "{0:g}".format(x / 1000) + "k"))
+    ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax1.set_xlabel("C-walks length", fontsize=16)
+    ax1.set_ylabel("Frequency of occurrence", fontsize=16)
+    ax2.set_xlabel("C-walks length", fontsize=16)
+    ax2.set_ylabel("Frequency of occurrence", fontsize=16)
+    return plt.savefig(f"{name}"), plt.close()
+
+
+def identical(cwalk):
+    span = 1
+    chrs = [node[2] for node in cwalk]
+    while not all(chrs[0] == element for element in chrs):
+        if not all(chrs[0] == element for element in chrs):
+            span += 1
+            repeat = chrs[0]
+            chrs = [chr for chr in chrs if chr != repeat]
+    return span
+
+
+def counting(graphs, length):
+    i = 0
+    one, two, many = 0, 0, 0
+    for graph in graphs:
+        for cwalk in list(nx.connected_components(graph)):
+            cwalk = list(cwalk)
+            if len(cwalk) == length:
+                i += 1
+                span = identical(cwalk)
+                if span == 1:
+                    one += 1
+                elif span == 2:
+                    two += 1
+                elif span >= 3:
+                    many += 1
+
+    return [round((one / i) * 100, 2), round((two / i) * 100, 2), round((many / i) * 100, 2)]
+
+
+def main():
+    graphs, labels = load_files(sys.argv[2], load_cwalk_graph)  # load cwalk graphs
+    one_chr, two_chr, many_chr = [], [], []
+    for hop in range(3, 16):
+        one, two, many = counting(graphs, hop)
+        one_chr.append(one)  # fraction of inter-chrs cwalks of particular length
+        two_chr.append(two)
+        many_chr.append(many)
+
+    """ Plotting """
+    stats(graphs, sys.argv[3])
+    fractions(one_chr, two_chr, many_chr, sys.argv[4])
+    barh(graphs, labels, sys.argv[5])
+
+
+if __name__ == '__main__':
+    main()
+
